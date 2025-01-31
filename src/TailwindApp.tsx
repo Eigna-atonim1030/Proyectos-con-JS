c81ce24a68642644d4548da1a83257bc87232508
SebastianDev807 <sebastian.dev0708@gmail.com>
2025-01-30 23:38:47 -0500

    First commit

---
+++ src/TailwindApp.tsx
@@ -0,0 +1,10 @@
+const TailwindApp = () => {
+    return (
+        <h1 className="text-5xl font-extrabold text-gray-800  text-center p-4">
+        ¡Welcome to Tailwind CSS! 🚀
+      </h1>
+      
+    )
+}
+
+export default TailwindApp
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './index.css'
+import TailwindApp from './TailwindApp'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <TailwindApp/>
+
+  </StrictMode>,
+)
